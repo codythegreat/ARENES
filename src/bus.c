98a0816d9bc40227cb257f793dc11bd620910299
codythegreat <maxiecody@gmail.com>
2019-11-18 18:29:11 +0000

    added logic for creating bus, writing to bus

--- src/bus.c
+++ src/bus.c
@@ -1,6 +1,9 @@
 #include "bus.h"
 
-
+bus *createBus(struct _6502 *cpu6502, struct ) {
+    bus *b = {cpu6502, };
+    return b;
+}
 uint8_t read(uint16_t address) {
     if (address >= 0x0000 && address <= 0xFFFF) {
         // TODO: map return values for each NES component
@@ -9,4 +12,10 @@ uint8_t read(uint16_t address) {
     return 0x00;
 }
 
-void write(uint16_t address, uint8_t data);
+void write(uint16_t address, uint8_t data) {
+    if (address >= 0x0000 && address <= 0xFFFF) {
+        bus->ram[address] = data;
+    } else {
+        // TODO: handle error
+    }
+}
