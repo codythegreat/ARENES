7d6185a7b50379f924e18f2954a65eab9d2cf11b
codythegreat <maxiecody@gmail.com>
2019-11-18 15:07:13 +0000

    begin building struct for 6502 proc

--- include/6502.h
+++ include/6502.h
@@ -2,13 +2,12 @@
 # define 6502_H
 
 
-
 /////////// FLAGS ///////////
 // the 6502 processor uses
 // a single byte to keep 
 // track of several key 
 // pieces of information
-enum FLAGS
+typedef enum
 {
     C = (1<<0), // Carry
     Z = (1<<1), // Zero
@@ -18,7 +17,32 @@ enum FLAGS
     U = (1<<5), // Unused
     V = (1<<6), // Overflow
     N = (1<<7), // Negative
-};
+} FLAGS;
+
+struct _6502 {
+    /////////// REGISTERS ///////////
+    // the 6502 processor has three
+    // primary registers, a (the 
+    // accumulator) is used for math
+    // operations, x and y are used
+    // for looping. They are mostly
+    // the same.
+    uint8_t a = 0x00;
+    uint8_t x = 0x00;
+    uint8_t y = 0x00;
+    // The stack points to current 
+    // location on the bus
+    uint8_t stackPtr = 0x00;
+    uint8_t programCounter = 0x00;
+    uint8_t status = 0x00;
+
+    // pointer to the bus so that
+    // our cpu can access the other
+    // components
+    struct bus *bus;
+
+    FLAGS flags;
+}
 
 /////////// Opcodes ///////////
 // there are 56 legal opcodes
